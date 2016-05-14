291b7cebd13fb53ad7244809f839669042a74872
Ian Webster <ianw_github@ianww.com>
2016-05-14 15:27:13 -0700

    more political post boilerplate

--- main.js
+++ main.js
@@ -5,7 +5,16 @@ $(function() {
 
 function addButtons() {
   $('.userContentWrapper').not('.stupid__donate-button-added').each(function() {
-    var $button = $('<div>').addClass('stupid__donate-button');
-    $(this).prepend($button).addClass('stupid__donate-button-added');
+    var $this = $(this);
+
+    var trigger = isPoliticalPost($this.text());
+    if (trigger) {
+      var $button = $('<div>').addClass('stupid__donate-button').text('Button');
+      $this.prepend($button).addClass('stupid__donate-button-added');
+    }
   });
+}
+
+function isPoliticalPost(postText) {
+  return true;
 }
