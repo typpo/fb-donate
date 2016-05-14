c76014c5c99b317898ac58b8bf23c411bb8c6a3a
Ian Webster <ianw_github@ianww.com>
2016-05-14 15:32:31 -0700

    Button stylin

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ function addButtons() {
   $('.userContentWrapper').not('.stupid__donate-button-added').each(function() {
     var $this = $(this);
 
+    if ($this.find('.userContentWrapper').length > 0) {
+      // This is just a wrapper wrapper -- ignore.
+      return true;
+    }
+
     var trigger = isPoliticalPost($this.text());
     if (trigger) {
       var $button = $('<div>').addClass('stupid__donate-button').text('Button');
