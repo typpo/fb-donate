9a0824bf5b1e79c5b49ecd45a1c85032618a0ff8
Ian Webster <ianw_github@ianww.com>
2016-05-14 15:21:56 -0700

    Insert something on every post on the page

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert("MAIN.JS");
+$(function() {
+  addButtons();
+  setInterval(addButtons, 1000);
+});
+
+function addButtons() {
+  $('.userContentWrapper').not('.stupid__donate-button-added').each(function() {
+    var $button = $('<div>').addClass('stupid__donate-button');
+    $(this).prepend($button).addClass('stupid__donate-button-added');
+  });
+}
